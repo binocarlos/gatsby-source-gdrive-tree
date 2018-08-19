241b029c4d0f6eb34f582d26badc92e3802807ca
Kai Davenport <kaiyadavenport@gmail.com>
2018-08-19 22:13:22 +0100

    include nocode data

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,17 +7,29 @@ const NODE_TYPES = {
   'application/vnd.google-apps.spreadsheet': nodes.Spreadsheet,
 }
 
+const dataFolderName = '_nocode_data'
+
 exports.sourceNodes = (
   { boundActionCreators },
-  { serviceAccountToken, folderId },
+  { 
+    serviceAccountToken, 
+    folderId,
+  },
   done
 ) => {
   const { createNode } = boundActionCreators
+
+  // a map of the file id -> the JSON file data for that item
+  const _nodecode_data = {}
 
   const nodeFactory = (item, parentNode) => {
     // get the nodeFactory based on the mimeType
     const nodeType = NODE_TYPES[item.mimeType]
     if(!nodeType) throw new Error(`unknown data mimeType: ${item.mimeType} - id: ${item.id} - name: ${item.name}`)
+
+    item.nocodeData = Object.assign({
+      noCode: true,
+    }, _nodecode_data[item.id])
 
     // create the node data using the factory
     // point at the parent node if given
@@ -47,6 +59,11 @@ exports.sourceNodes = (
 
   }
 
+  // is the given item the data folder?
+
+  const isDataFolder = (item) => item.mimeType == 'application/vnd.google-apps.folder' && item.name == dataFolderName
+  const getDataFolder = (topItems) => topItems.filter(isDataFolder)[0]
+
   // load the data tree from google drive
   googleDriveLoader({
     serviceAccountToken,
@@ -55,9 +72,21 @@ exports.sourceNodes = (
   }, (err, results) => {
     if(err) return done(err)
 
+    // get a reference to the data folder
+    const dataFolder = getDataFolder(results)
+
+    if(dataFolder) {
+      dataFolder.contents.forEach(dataFileItem => {
+        const itemId = dataFileItem.name.split('.')[0]
+        _nodecode_data[itemId] = dataFileItem.contents
+      })
+    }
+
+    const topLevelItems = results.filter(result => isDataFolder(result) ? false : true)
+
     // loop over the root items
     // we don't pass a parent because these are at the top
-    results.forEach(item => nodeFactory(item))
+    topLevelItems.forEach(item => nodeFactory(item))
     done()
   })
 }
